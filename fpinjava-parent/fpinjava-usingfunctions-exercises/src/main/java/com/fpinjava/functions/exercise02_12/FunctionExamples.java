064f1c016cb0166a7546072bb700e96357116e2b
Marcel Englmaier <saurutobi@gmail.com>
2020-06-01 20:03:53 -0700

    2.12

--- fpinjava-parent/fpinjava-usingfunctions-exercises/src/main/java/com/fpinjava/functions/exercise02_12/FunctionExamples.java
+++ fpinjava-parent/fpinjava-usingfunctions-exercises/src/main/java/com/fpinjava/functions/exercise02_12/FunctionExamples.java
@@ -6,17 +6,17 @@ public class FunctionExamples {
    * Note: The same implementation may be used for all four cases.
    */
 
-  public static final Function<Integer, Integer> factorial0 = null; // To be implemented
+  public static final Function<Integer, Integer> factorial0 = x -> x==0 ? 1 : x * FunctionExamples.factorial0.apply(x - 1);
 
   public static Function<Integer, Integer> factorial1;
   static {
-    factorial1 = null; // To be implemented
+    factorial1 = x -> x==0 ? 1 : x * factorial1.apply(x - 1);
   }
 
-  public final Function<Integer, Integer> factorial2 = null; // To be implemented
+  public final Function<Integer, Integer> factorial2 = x -> x==0 ? 1 : x * this.factorial2.apply(x - 1);
 
   public Function<Integer, Integer> factorial3;
   {
-    factorial3 = null; // To be implemented
+    factorial3 = x -> x==0 ? 1 : x * this.factorial3.apply(x - 1);
   }
 }
